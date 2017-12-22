9c6a4f142fec0fbcc1461751cbe0b7dbba5e6f5c
Laurent Gaches <laurent@binimo.com>
2017-12-22 11:45:02 +0100

    update dep

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         ],
     dependencies: [
         .package(url: "https://github.com/bermudadigitalstudio/Titan.git", .exact( "0.9.0-beta.3")),
-        .package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", .exact( "0.9.0-beta.4")),
+        .package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", .branch("develop")),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git",.upToNextMinor(from: "1.5.0")),
     ],
     targets: [
